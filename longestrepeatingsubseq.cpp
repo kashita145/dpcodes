df0541dbc9908999fcf5642b726dbfb349acd1a4
kashita145 <60257324+kashita145@users.noreply.github.com>
2021-01-29 14:56:26 +0530

    Update longestrepeatingsubseq.cpp

--- longestrepeatingsubseq.cpp
+++ longestrepeatingsubseq.cpp
@@ -78,9 +78,7 @@ string lcs(string a , string b, int n, int m){
 
     for(int i=1;i<=n;i++){
         for(int j=1;j<=n;j++){
-            if(i==j)
-                continue;
-            if(a[i-1]==b[j-1])
+            if(a[i-1]==b[j-1] && i!=j)
                 dp[i][j]=dp[i-1][j-1]+1;
             else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
         }
